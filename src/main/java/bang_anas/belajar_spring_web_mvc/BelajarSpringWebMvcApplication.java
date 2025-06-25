4de77bc17c6d1ce66236bb9ca033f2689b8aa12c
Khairunas R W <128572429+khairunas001@users.noreply.github.com>
2025-06-25 22:20:10 +0700

    25-6-2025

---
+++ src/main/java/bang_anas/belajar_spring_web_mvc/BelajarSpringWebMvcApplication.java
@@ -0,0 +1,13 @@
+package bang_anas.belajar_spring_web_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringWebMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringWebMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bang_anas/belajar_spring_web_mvc/BelajarSpringWebMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package bang_anas.belajar_spring_web_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringWebMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
